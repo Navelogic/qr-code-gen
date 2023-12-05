fb8274f9fb71d751f6b138bd962ce925bdccc0da
Arian Weslley <weslleysantoszbd@gmail.com>
2023-12-05 16:04:50 -0300

    Refatoração para padrão Spring

--- src/main/java/br/com/qrcodegen/qrcodegen/QrCodeGenApplication.java
+++ src/main/java/br/com/qrcodegen/qrcodegen/QrCodeGenApplication.java
@@ -1,19 +1,10 @@
 package br.com.qrcodegen.qrcodegen;
 
-import java.util.Scanner;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.google.zxing.common.StringUtils;
-
-import lombok.extern.slf4j.Slf4j;
-
 @SpringBootApplication
 public class QrCodeGenApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(QrCodeGenApplication.class, args);
