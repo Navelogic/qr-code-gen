db02c7950f2431ec27cb803ac006161b3d882cf0
Arian Weslley <weslleysantoszbd@gmail.com>
2023-12-05 16:02:06 -0300

    Finalizando Service

--- src/main/java/br/com/qrcodegen/qrcodegen/QrCodeGenService.java
+++ src/main/java/br/com/qrcodegen/qrcodegen/QrCodeGenService.java
@@ -15,44 +15,48 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.common.StringUtils;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class QrCodeGenService {
 
-    @Value("${qrcode.output.dictory}")
-    private String outputDictory;
+    @Value("${qrcode.message}")
+    private String qrCodeMessage;
+
+    @Value("${qrcode.output.directory}")
+    private String outputLocation;
 
     private static final String charset = "UTF-8";
     private static final String strDateFormat = "ddMMyyyyHHmmss";
 
+
+
     public void genQrCode(String text) {
-        System.out.println("Gerando QR Code...");
-        System.out.println("Diretório de saída: " + outputDictory);
+        log.info("Gerando QRCode {}", text);
+
+        log.info("Diretório de saída: {}", outputLocation);
 
         try{
-            String finalMessage = (StringUtils.isNotBlank(text))?text:"";
-            System.out.println("Texto digitado: " + finalMessage);
-            processarQRCode(finalMessage, prepareOutputFileName(), charset, 400, 400);
-        } catch (WriterException e){
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
+            String finalText = qrCodeMessage + text;
+            log.info("Texto final: {}", finalText);
+            processarQRCode(finalText, prepareOutputFileName(), charset, 400, 400);
+
+        } catch (Exception e){
+            log.error("Erro ao gerar QRCode", e);
         }
     }
 
     private String prepareOutputFileName() {
-        Date date = new Date();
-
         DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
-        String formattedDate = dateFormat.format(date);
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(outputDictory).append("\\").append("QRCode-").append(formattedDate).append(".png");
-        System.out.println("Nome do arquivo de saída: " + sb.toString());
-        return sb.toString();
+        Date date = new Date(System.currentTimeMillis());
+        return outputLocation + dateFormat.format(date) + ".png";
     }
 
-    private void processarQRCode(String data, String filePath, String charset, int qrCodeheight, int qrCodewidth) throws WriterException, IOException {
-        BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset), BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight);
+    private void processarQRCode(String qrCodeData, String filePath, String charset, int qrCodeheight, int qrCodewidth)
+            throws WriterException, IOException {
+        BitMatrix matrix = new MultiFormatWriter().encode(new String(qrCodeData.getBytes(charset), charset),
+                BarcodeFormat.QR_CODE, qrCodewidth, qrCodeheight);
         MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath.lastIndexOf('.') + 1), new java.io.File(filePath));
     }
 }
