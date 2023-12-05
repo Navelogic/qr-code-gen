2709c58c8d311bb327681bef8bc740afb44fc172
Arian Weslley <weslleysantoszbd@gmail.com>
2023-12-05 15:02:58 -0300

    Iniciando Projeto

---
+++ src/main/java/br/com/qrcodegen/qrcodegen/QrCodeGenApplication.java
@@ -0,0 +1,13 @@
+package br.com.qrcodegen.qrcodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCodeGenApplication.class, args);
+    }
+
+}
